218d0776b8433f1668d4b871e0243c89a166820d
Nathan Tyler <croixhaug@gmail.com>
2015-04-02 12:57:23 -0700

    Optimize detail image size
    Asana #30246587612142

--- public/includes/pro/pro.php
+++ public/includes/pro/pro.php
@@ -11,6 +11,7 @@ class DrawAttention_Pro {
 
 		remove_shortcode( 'drawattention' );
 		add_shortcode( 'drawattention', array( $this, 'shortcode' ) );
+		add_filter( 'da_detail_image_size', array( $this, 'optimize_detail_image_size' ), 10, 4 );
 	}
 
 	function add_layout_metabox( $metaboxes ) {
@@ -53,6 +54,18 @@ class DrawAttention_Pro {
 		);
 
 		return $metaboxes;
+	}
+
+	public function optimize_detail_image_size( $size, $hotspot, $image, $settings ) {
+		$layout = $settings[$this->parent->custom_fields->prefix.'map_layout'][0];
+
+		if ( in_array( $layout, array( 'top', 'bottom', 'lightbox' ) ) ) {
+			$size = 'large';
+		} elseif ( in_array( $layout, array( 'left', 'right' ) ) ) {
+			$size = 'medium';
+		}
+
+		return $size;
 	}
 
 	public function shortcode( $atts ) {
@@ -163,7 +176,7 @@ class DrawAttention_Pro {
 				$html .=    '<h2 class="hotspot-title">' . $hotspot['title'] . '</h2>';
 				if ( !empty( $hotspot['detail_image'] ) ) {
 					$html .=  '<div class="hotspot-thumb">';
-					$html .=    '<img src="'.$hotspot['detail_image'].'" />';
+					$html .=    wp_get_attachment_image( $hotspot['detail_image_id'], apply_filters( 'da_detail_image_size', 'large', $hotspot, $img_post, $settings ) );
 					$html .=  '</div>';
 				}
 				$html .=    '<div class="hotspot-content">' . wpautop( $hotspot['description'] ) . '</div>';
