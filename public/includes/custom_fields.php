bef139d1fe8837f82820c2c43f15cd4a4fdd40dd
Nathan Tyler <croixhaug@gmail.com>
2015-06-09 11:55:59 -0700

    Change description field to WYSIWYG

--- public/includes/custom_fields.php
+++ public/includes/custom_fields.php
@@ -244,21 +244,21 @@ class DrawAttention_CustomFields {
 							'name' => __('Description', 'drawattention' ),
 							'description' => '',
 							'id'   => 'description',
-							'type' => 'textarea_small',
-							// 'type' => 'wysiwyg',
-							// 'options' => array(
-							// 	// 'wpautop' => true, // use wpautop?
-							// 	'media_buttons' => false, // show insert/upload button(s)
-							// 	// 'textarea_name' => $editor_id, // set the textarea name to something different, square brackets [] can be used here
-							// 	'textarea_rows' => get_option('default_post_edit_rows', 7), // rows="..."
-							// 	// 'tabindex' => '',
-							// 	// 'editor_css' => '', // intended for extra styles for both visual and HTML editors buttons, needs to include the `<style>` tags, can use "scoped".
-							// 	// 'editor_class' => '', // add extra class(es) to the editor textarea
-							// 	'teeny' => true, // output the minimal editor config used in Press This
-							// 	// 'dfw' => false, // replace the default fullscreen with DFW (needs specific css)
-							// 	// 'tinymce' => true, // load TinyMCE, can be used to pass settings directly to TinyMCE using an array()
-							// 	// 'quicktags' => true // load Quicktags, can be used to pass settings directly to Quicktags using an array()
-							// ),
+							// 'type' => 'textarea_small',
+							'type' => 'wysiwyg',
+							'options' => array(
+								// 'wpautop' => true, // use wpautop?
+								'media_buttons' => false, // show insert/upload button(s)
+								// 'textarea_name' => $editor_id, // set the textarea name to something different, square brackets [] can be used here
+								'textarea_rows' => get_option('default_post_edit_rows', 7), // rows="..."
+								// 'tabindex' => '',
+								// 'editor_css' => '', // intended for extra styles for both visual and HTML editors buttons, needs to include the `<style>` tags, can use "scoped".
+								// 'editor_class' => '', // add extra class(es) to the editor textarea
+								'teeny' => true, // output the minimal editor config used in Press This
+								// 'dfw' => false, // replace the default fullscreen with DFW (needs specific css)
+								// 'tinymce' => true, // load TinyMCE, can be used to pass settings directly to TinyMCE using an array()
+								// 'quicktags' => true // load Quicktags, can be used to pass settings directly to Quicktags using an array()
+							),
 						),
 						array(
 							'name' => __( 'Detail Image', 'drawattention' ),
