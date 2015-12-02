6a68cd62eff9df979bed068deef35f252a1bf176
NatalieMac <natalie@nataliemac.com>
2015-12-02 10:48:04 -0800

    Revert "Change from throttle to debounce for window resize"
    This reverts commit 1b7f45f40baf7287f0a2df18cc3814a57bd590ac.

--- public/assets/js/jquery.responsilight.js
+++ public/assets/js/jquery.responsilight.js
@@ -447,6 +447,19 @@
 		}
 	};
 
+	resizeDelay = (function() {
+		var timers = {};
+		return function (callback, ms, uniqueId) {
+			if (!uniqueId) {
+				uniqueId = Math.random() * 100;
+			}
+			if (timers[uniqueId]) {
+				clearTimeout (timers[uniqueId]);
+			}
+			timers[uniqueId] = setTimeout(callback, ms);
+		};
+	})();
+
 	simpleMap = function(map) {
 		map.find('area').each(function(e){
 			e.preventDefault();
@@ -472,8 +485,10 @@
 			});
 		}
 
-		$(window).on('smartresize orientationchange', function() {
-			responsilight_init();
+		$(window).on('resize orientationchange', function() {
+			resizeDelay(function() {
+				responsilight_init();
+			}, 300, 'responsilight');
 		}).trigger('resize');
 
 		return this;
@@ -489,27 +504,3 @@
 	}
 
 }(jQuery, window, document));
-
-
-// debouncing function from John Hann
-// http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
-(function($,sr){
-  var debounce = function (func, threshold, execAsap) {
-      var timeout;
-      return function debounced () {
-          var obj = this, args = arguments;
-          function delayed () {
-              if (!execAsap)
-                  func.apply(obj, args);
-              timeout = null;
-          };
-          if (timeout)
-              clearTimeout(timeout);
-          else if (execAsap)
-              func.apply(obj, args);
-          timeout = setTimeout(delayed, threshold || 100);
-      };
-  }
-  // smartresize
-  jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
-})(jQuery,'smartresize');
