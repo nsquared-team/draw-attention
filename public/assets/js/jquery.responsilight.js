32f988a59c41b18938bb9368b15a9214230a1ff4
NatalieMac <natalie@nataliemac.com>
2015-12-02 10:47:56 -0800

    Revert "Fix debounce"
    This reverts commit 0b0c3c3c453cdb351e8817e3f21d241e434ecf85.

--- public/assets/js/jquery.responsilight.js
+++ public/assets/js/jquery.responsilight.js
@@ -317,8 +317,8 @@
 				mapOver(area, img, null);
 				mapClick(area, img);
 				setTimeout(function() {
-					window.scrollTo(0, scrollCoord);
-				}, 1);
+    			window.scrollTo(0, scrollCoord);
+  			}, 1);
 			}
 		}
 	};
@@ -474,7 +474,7 @@
 
 		$(window).on('smartresize orientationchange', function() {
 			responsilight_init();
-		}).trigger('smartresize');
+		}).trigger('resize');
 
 		return this;
 	};
@@ -494,22 +494,22 @@
 // debouncing function from John Hann
 // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
 (function($,sr){
-	var debounce = function (func, threshold, execAsap) {
-		var timeout;
-		return function debounced () {
-			var obj = this, args = arguments;
-			function delayed () {
-				if (!execAsap)
-					func.apply(obj, args);
-				timeout = null;
-			};
-			if (timeout)
-				clearTimeout(timeout);
-			else if (execAsap)
-				func.apply(obj, args);
-			timeout = setTimeout(delayed, threshold || 100);
-		};
-	}
-	// smartresize
-	jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
+  var debounce = function (func, threshold, execAsap) {
+      var timeout;
+      return function debounced () {
+          var obj = this, args = arguments;
+          function delayed () {
+              if (!execAsap)
+                  func.apply(obj, args);
+              timeout = null;
+          };
+          if (timeout)
+              clearTimeout(timeout);
+          else if (execAsap)
+              func.apply(obj, args);
+          timeout = setTimeout(delayed, threshold || 100);
+      };
+  }
+  // smartresize
+  jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
 })(jQuery,'smartresize');
