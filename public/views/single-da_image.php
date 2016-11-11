34b12e743e7fe080104563cd00763d27b40868a4
Nathan Tyler <croixhaug@gmail.com>
2016-11-11 10:45:09 -0800

    Fix password protection on single da-image view

--- public/views/single-da_image.php
+++ public/views/single-da_image.php
@@ -10,8 +10,10 @@ get_header(); ?>
 		<div id="content" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
-				<?php if ( post_password_required() ): ?>
+				<?php if ( ! post_password_required() ): ?>
 					<?php echo do_shortcode( '[drawattention ID="'.get_the_id().'"]' ); ?>
+				<?php else: ?>
+					<?php the_content(); ?>
 				<?php endif ?>
 				<?php if ( current_user_can( 'edit_posts' ) ): ?>
 					<?php edit_post_link( __( 'Edit Interactive Image', 'drawattention' ) ); ?>
