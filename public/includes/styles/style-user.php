bd937111646811ce51a11ac60e5227412043af57
Nathan Tyler <croixhaug@gmail.com>
2017-06-27 09:26:23 -0700

    Fix PHP warning in  user styles PHP

--- public/includes/styles/style-user.php
+++ public/includes/styles/style-user.php
@@ -1,8 +1,12 @@
 <?php
 class DrawAttention_User_Style extends DrawAttention_Style {
 	function get_saved_styles( $post_id = null ) {
-		if ( empty( $post_id ) ) {
+		if ( empty( $post_id ) && !empty( $_GET['post'] ) ) {
 			$post_id = esc_attr( $_GET['post'] );
+		}
+
+		if ( empty( $post_id ) ) {
+			return array();
 		}
 		$saved_user_styles = get_post_meta( $post_id, '_da_styles', true );
 		if ( empty( $saved_user_styles['0']['title'] ) ) {
