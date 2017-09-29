a73b045b4e5e5319def663adb83e55b940f385b3
Nathan Tyler <croixhaug@gmail.com>
2017-09-29 11:38:59 -0700

    1.8.5 version number

--- public/class-drawattention.php
+++ public/class-drawattention.php
@@ -30,7 +30,7 @@ if ( !class_exists( 'DrawAttention' ) ) {
 		 *
 		 * @var     string
 		 */
-		const VERSION = '1.8.4';
+		const VERSION = '1.8.5';
 		const file = __FILE__;
 		const name = 'Draw Attention';
 		const slug = 'drawattention';
