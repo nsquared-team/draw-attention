da762081fa4905531194c7de4fb01bc477593910
NatalieMac <natalie@nataliemac.com>
2018-05-15 15:49:41 -0700

    Change version to 1.7.0

--- draw-attention.php
+++ draw-attention.php
@@ -9,7 +9,7 @@
 Plugin Name:       Draw Attention
 Plugin URI:        http://wpdrawattention.com
 Description:       Create interactive images in WordPress
-Version:           1.6.15
+Version:           1.7.0
 Author:            Tyler Digital & Purple Pen Productions
 Author URI:        http://wpdrawattention.com
 Text Domain:       drawattention
