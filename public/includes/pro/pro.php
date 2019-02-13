297b1d25fe19bef83a58f704f1dd2ef4be93856a
Nathan Tyler <croixhaug@gmail.com>
2019-02-12 16:06:21 -0800

    Fix undefined index

--- public/includes/pro/pro.php
+++ public/includes/pro/pro.php
@@ -143,11 +143,11 @@ class DrawAttention_Pro {
 		}
 		$settings['styles'][] = array(
 			'title' => 'default',
-			'map_highlight_color' => $settings['img_settings'][$this->parent->custom_fields->prefix.'map_highlight_color'][0],
-			'map_highlight_opacity' => $settings['img_settings'][$this->parent->custom_fields->prefix.'map_highlight_opacity'][0],
-			'map_border_color' => $settings['img_settings'][$this->parent->custom_fields->prefix.'map_border_color'][0],
-			'_da_map_hover_color' => $settings['img_settings'][$this->parent->custom_fields->prefix.'map_hover_color'][0],
-			'_da_map_hover_opacity' => $settings['img_settings'][$this->parent->custom_fields->prefix.'map_hover_opacity'][0]
+			'map_highlight_color' => !empty( $settings['img_settings'][$this->parent->custom_fields->prefix.'map_highlight_color'][0] ) ? $settings['img_settings'][$this->parent->custom_fields->prefix.'map_highlight_color'][0] : '',
+			'map_highlight_opacity' => !empty( $settings['img_settings'][$this->parent->custom_fields->prefix.'map_highlight_opacity'][0] ) ? $settings['img_settings'][$this->parent->custom_fields->prefix.'map_highlight_opacity'][0] : '',
+			'map_border_color' => !empty( $settings['img_settings'][$this->parent->custom_fields->prefix.'map_border_color'][0] ) ? $settings['img_settings'][$this->parent->custom_fields->prefix.'map_border_color'][0] : '',
+			'_da_map_hover_color' => !empty( $settings['img_settings'][$this->parent->custom_fields->prefix.'map_hover_color'][0] ) ? $settings['img_settings'][$this->parent->custom_fields->prefix.'map_hover_color'][0] : '',
+			'_da_map_hover_opacity' => !empty( $settings['img_settings'][$this->parent->custom_fields->prefix.'map_hover_opacity'][0] ) ? $settings['img_settings'][$this->parent->custom_fields->prefix.'map_hover_opacity'][0] : ''
 		);
 
 		// Create formatted array of styles
