52d2a8130bca34fbda1d2ccb3730a454f2e3b6f6
Nathan Tyler <croixhaug@gmail.com>
2019-11-19 12:22:19 -0800

    Port BigCommerce from Pro

--- public/includes/actions/action.php
+++ public/includes/actions/action.php
@@ -6,6 +6,10 @@ class DrawAttention_Action {
 		add_filter( 'da_hotspot_area_group_details', array( $this, 'add_action_fields' ), $this->priority, 1 );
 	}
 
+	public static function is_active() {
+		return true;
+	}
+
 	function add_action_fields( $group_details ) {
 		throw new Exception( 'Custom DrawAttention_Action objects need to implement add_action_fields()' );
 
