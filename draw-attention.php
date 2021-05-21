6c69951220e4f17434dcd9dbf73a32a5f3aef9ee
Ken Gagne <ken.gagne@automattic.com>
2021-05-21 10:40:36 -0700

    Corrects GitHub URL
    Adds a missing dash to the GitHub URL referenced in the plugin's header

--- draw-attention.php
+++ draw-attention.php
@@ -16,7 +16,7 @@ Text Domain:       draw-attention
 License:           GPL-2.0+
 License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
 Domain Path:       /languages
-GitHub Plugin URI: https://github.com/tylerdigital/drawattention/
+GitHub Plugin URI: https://github.com/tylerdigital/draw-attention
 */
 
 // If this file is called directly, abort.
