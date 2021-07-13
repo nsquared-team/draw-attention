7a6adf56a046a3bc50453e56e16598556cbc32d2
Nathan Tyler <croixhaug@gmail.com>
2021-07-12 22:05:32 -0700

    1.9.20

--- public/class-drawattention.php
+++ public/class-drawattention.php
@@ -31,7 +31,7 @@ if ( !class_exists( 'DrawAttention' ) ) {
 		 *
 		 * @var     string
 		 */
-		const VERSION = '1.9.19';
+		const VERSION = '1.9.20';
 		const file = __FILE__;
 		const name = 'Draw Attention';
 		const slug = 'drawattention';
