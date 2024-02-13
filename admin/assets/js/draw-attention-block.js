59f2856652dd1734f5f92fc438040d306ba82ca4
hsein-bitar <hseinbitar22@gmail.com>
2024-02-13 17:14:43 +0200

    working solution

--- admin/assets/js/draw-attention-block.js
+++ admin/assets/js/draw-attention-block.js
@@ -1,31 +1,30 @@
-(function(blocks, element, serverSideRender, blockEditor )
-	{
-	var el = element.createElement,
-		registerBlockType = blocks.registerBlockType,
-		ServerSideRender = serverSideRender,
-		useBlockProps = blockEditor.useBlockProps;
+(function (blocks, element, serverSideRender, blockEditor) {
+  var el = element.createElement,
+    registerBlockType = blocks.registerBlockType,
+    ServerSideRender = serverSideRender,
+    useBlockProps = blockEditor.useBlockProps;
 
-	registerBlockType( 'draw-attention/image', {
-		apiVersion: 2,
-		title: 'Draw Attention Image',
-		icon: 'images-alt2',
-		category: 'widgets',
+  registerBlockType("draw-attention/image", {
+    apiVersion: 2,
+    title: "Draw Attention Image",
+    icon: "images-alt2",
+    category: "widgets",
 
-		edit: function(props) {
-			var blockProps = useBlockProps();
-			return el(
-				'div',
-				blockProps,
-				el(ServerSideRender, {
-					block: 'draw-attention/image',
-					attributes: props.attributes,
-				})
-			);
-		},
-	} );
+    edit: function (props) {
+      var blockProps = useBlockProps();
+      return el(
+        "div",
+        blockProps,
+        el(ServerSideRender, {
+          block: "draw-attention/image",
+          attributes: props.attributes,
+        }),
+      );
+    },
+  });
 })(
-	window.wp.blocks,
-	window.wp.element,
-	window.wp.serverSideRender,
-	window.wp.blockEditor
+  window.wp.blocks,
+  window.wp.element,
+  window.wp.serverSideRender,
+  window.wp.blockEditor,
 );
