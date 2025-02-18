d8b576579e96b890186cc024cbbee6a7614354e1
tylerdigital <nathan@tylerdigital.com>
2025-02-18 17:06:22 +0000

    Update to 2.0.31 [version-bump]

--- draw-attention.php
+++ draw-attention.php
@@ -9,7 +9,7 @@
 Plugin Name:       Draw Attention
 Plugin URI:        https://wpdrawattention.com
 Description:       Create interactive images in WordPress
-Version:           2.0.30
+Version:           2.0.31
 Author:            NSquared
 Author URI:        https://nsquared.io
 Text Domain:       draw-attention
--- public/class-drawattention.php
+++ public/class-drawattention.php
@@ -31,7 +31,7 @@ if ( ! class_exists( 'DrawAttention' ) ) {
 		 *
 		 * @var     string
 		 */
-		const VERSION = '2.0.30';
+		const VERSION = '2.0.31';
 		const file    = __FILE__;
 		const name    = 'Draw Attention';
 		const slug    = 'drawattention';
