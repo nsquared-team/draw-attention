a16d4ec8976c5d16e726eb914503243930deb89c
Hsein Bitar <hseinbitar22@gmail.com>
2025-08-15 16:25:33 +0300

    trigger ci

--- public/assets/js/public.js
+++ public/assets/js/public.js
@@ -33,6 +33,7 @@
 
   // Intersection Observer to detect when hidden hotspots become visible
   var hotspotVisibilityObserver = null;
+  
 
   var setupHotspotVisibilityObserver = function () {
     if (!window.IntersectionObserver) {
