73485639c01e5791813501d9e045b21b44280def
otto-bot-git <otto@nsquared.io>
2025-10-21 12:31:23 +0000

    Automated code formatting on branch done.

--- public/includes/bb/bb.php
+++ public/includes/bb/bb.php
@@ -13,17 +13,18 @@ add_action( 'init', 'da_load_module_examples' );
 
 /* Define a custom field for select DA images */
 function da_select_img( $name, $value, $field ) {
-	$args = array(
-		'post_type'	=> 'da_image',
-		'post_status' => 'publish',
-		'posts_per_page' => -1
+	$args   = array(
+		'post_type'      => 'da_image',
+		'post_status'    => 'publish',
+		'posts_per_page' => -1,
 	);
-	$images = new WP_Query($args);
+	$images = new WP_Query( $args );
 
 	if ( $images->have_posts() ) {
-	echo '<select name="' . $name . '">';
+		echo '<select name="' . $name . '">';
 		echo '<option value="">Select an image</option>';
-		while ( $images->have_posts() ) { $images->the_post();
+		while ( $images->have_posts() ) {
+			$images->the_post();
 			$selected = $value == get_the_id() ? 'selected ' : '';
 			echo '<option ' . $selected . 'value="' . get_the_id() . '">' . get_the_title() . '</option>';
 		}
@@ -31,4 +32,3 @@ function da_select_img( $name, $value, $field ) {
 	}
 }
 add_action( 'fl_builder_control_select-img', 'da_select_img', 1, 3 );
-
